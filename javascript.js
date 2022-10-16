b475a8b9ee4add618453e7cf333722e3a4ae8bc8
John Kelly <jpkjpk98@me.com>
2022-10-15 23:25:15 -0700

    Complete first iteration of Rock, Paper, Scissors

---
+++ javascript.js
@@ -0,0 +1,118 @@
+//Create function that will getComputerChoice of rock, paper, or scissors
+let getComputerChoice = () => {
+    //Declare variable computerChoice and initialize to empty string
+    let computerChoice = "";
+    //Generate a random number between 0 and 9 that will be used for deciding computer's choice
+    let randomNumber = Math.floor(Math.random() * 10);
+    //Use while loop to ensure randomNumber is between 0-8 so we can properly allocate an equal chance to each possible choice (0-2 = Rock, 3-5 = Paper, 
+    //6-8 = Scissors, 9 = get a new randomNumber)
+    while (randomNumber === 9) {
+        randomNumber = Math.floor(Math.random() * 10);
+    }
+    //IF randomNumber is betwen 0-2 then set computerChoice to rock
+        //ELSE IF randomNumber is between 3-5 then set computerChoice to paper
+        //ELSE IF randomNumber is betwwen 6-8 then set computerChoice to scissors
+    if (randomNumber <= 2) {
+        return computerChoice = "Rock";
+    } else if (randomNumber <= 5) {
+        return computerChoice = "Paper";
+    } else if (randomNumber <= 8) {
+        return computerChoice = "Scissors";
+    }
+}
+
+//Create function with two parameters, playerInput and computerSelection, that plays a round
+let playRound = (playerInput, computerSelection) => {
+    //Set computerSelection to the result of getComputerChoice
+    //let computerSelection = getComputerChoice();********
+    //Set playerInput arguement to new variable playerSelection that converts to lower case to bypass case sensitivity 
+    let playerSelection = playerInput.toLowerCase();
+    console.log( "The computer chose: " + computerSelection);
+    //Create possible win and loss conditions as per rock/paper/scissors game's rules
+    if (computerSelection === "Rock" & playerSelection === "rock") {
+        return "You both chose Rock! It's a tie!"
+    } else if (computerSelection === "Rock" & playerSelection === "paper") {
+        return "You win! Paper beats Rock!"
+    } else if (computerSelection === "Rock" & playerSelection === "scissors") {
+        return "You lose! Rock beats Scissors!"
+    } else if (computerSelection === "Paper" & playerSelection === "paper") {
+        return "You both chose Paper! It's a tie!"
+    } else if (computerSelection === "Paper" & playerSelection === "rock") {
+        return "You lose! Paper beats Rock!"
+    } else if (computerSelection === "Paper" & playerSelection === "scissors") {
+        return "You win! Scissors beats Paper!"
+    } else if (computerSelection === "Scissors" & playerSelection === "scissors") {
+        return "You both chose Scissors! It's a tie!"
+    } else if ( computerSelection === "Scissors" & playerSelection === "rock") {
+        return "You win! Rock beats Scissors!"
+    } else if ( computerSelection === "Scissors" & playerSelection === "paper") {
+        return "You lose! Scissors beats paper!"
+    }
+}
+
+let playGame = () => {
+    //Declare var computerWins, playerWins, and ties to keep track of score
+    let computerWins = 0;
+    let playerWins = 0;
+    let ties = 0;
+    //Iterate through a certain amount of rounds
+    for (let i = 0; i < 5; i++) {
+        //Prompt user to input a string of rock, paper, or scissors and store value as lower case string (to bypass case sensitivity) in playerSelection
+        let playerSelection = prompt("Please type Rock, Paper, or Scissors: ").toLowerCase();
+        console.log(playerSelection);
+        //Declare var isPlayerSelectionValid and initialize to false
+        let isPlayerSelectionValid = false;
+        console.log("is valid: " + isPlayerSelectionValid)
+        //Check to see if playerSelection is valid
+        if (playerSelection === "rock") {
+          isPlayerSelectionValid = true;  
+        } else if (playerSelection === "paper") {
+            isPlayerSelectionValid = true;
+        } else if (playerSelection === "scissors") {
+            isPlayerSelectionValid = true;
+        }
+        console.log("is valid: " + isPlayerSelectionValid)
+        
+        //Create while loop that prompts user for valid input
+        while (isPlayerSelectionValid === false) {
+            playerSelection = prompt('No, you goofnut! You can only type Rock, Paper, or Scissors.').toLowerCase();
+            if (playerSelection === "rock") {
+                isPlayerSelectionValid = true;  
+              } else if (playerSelection === "paper") {
+                  isPlayerSelectionValid = true;
+              } else if (playerSelection === "scissors") {
+                  isPlayerSelectionValid = true;
+              }
+        }
+        //Display current round number
+        console.log(`Round ${i + 1}:`)
+        //Generate computerSelection for this round
+        let computerSelection = getComputerChoice();
+        //Store roundResult
+        let roundResult = playRound(playerSelection, computerSelection);
+        //Display results of this round
+       console.log(roundResult);
+       //Increase playerWins, computerWins, or ties by 1 depending on the victor of the round
+       if (roundResult.includes("win")) {
+        playerWins += 1;
+       } else if (roundResult.includes("lose")) {
+        computerWins += 1;
+       } else { 
+        ties += 1;
+       }
+       //Display message displaying current values of playerWins, computerWins, and ties
+       console.log(`You have won ${playerWins} rounds. 
+       The computer has won ${computerWins} rounds.
+       There are ${ties} ties.`)
+    }
+    //Display a message declaring the winner of the game
+    if (computerWins > playerWins) {
+        console.log(`You lost the game!`)
+    } else if (playerWins > computerWins) {
+        console.log(`You won the game!`)
+    } else {
+        console.log('Nobody won the game. It\'s a tie!');
+    }
+}
+
+playGame();
