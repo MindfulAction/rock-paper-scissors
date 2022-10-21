5f56eba15a086822d5c8e057f8d1f5f2f9ff8665
John Kelly <jpkjpk98@me.com>
2022-10-20 18:02:08 -0700

    Finish adding UI & mostly finish cleaning code

--- javascript.js
+++ javascript.js
@@ -27,38 +27,56 @@ let initializeGame = () => {
     ties.textContent = 0;
     document.querySelector(".paper").style.display = "";  
     document.querySelector(".scissors").style.display = "";
+    document.querySelector(".rock").style.display = "";
+    results.textContent = "LETS GET READY TO ROCK!!!! (or paper, or scissors)"
+
 
 }
 
 //NEED TO FINISH RESTART FUNCTION AND CLEAN UP CODE
 
-let restartGamePrompt = () => {
-    document.querySelector(".paper").style.display = "none";  
+let hidePlayerChoices = () => {
+    document.querySelector(".rock").style.display = "none";
+    document.querySelector(".paper").style.display = "none";
     document.querySelector(".scissors").style.display = "none";
-    let restartButton = document.querySelector(".rock");
-    restartButton.textContent = "Play Again";
-    restartButton.addEventListener("click", initializeGame())
-
 }
 
-let checkIfGameOver = (playerScore, computerScore) => {
+let restartGamePrompt = () => {
+    hidePlayerChoices();
+    const restartInstructions = results;
+    restartInstructions.textContent = "Press ENTER to Play Again";
+    document.addEventListener("keydown", (key) => {
+        if (key.keyCode == 13) {
+            initializeGame();
+        }
+    })
+}
 
-    if (playerScore == 5) {
+let checkIfGameOver = () => {
+    if (playerScore.textContent == 5) {
         results.textContent = `YOU WIN!!`;
-    } else if (computerScore == 5) {
+    } else if (computerScore.textContent == 5) {
         results.textContent = `Sorry, you lost.`;
     }
-    if (playerScore == 5 || computerScore == 5) {
+    if (playerScore.textContent == 5 || computerScore.textContent == 5) {
         restartGamePrompt();
     }
 }
 
-//Create function with two parameters, playerInput and computerSelection, that plays a round
+let updateScore = () => {
+    if (results.textContent.includes("win")) {
+        playerScore.textContent = parseInt(playerScore.textContent) + 1
+       } else if (results.textContent.includes("lose")) {
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
+       } else { 
+        ties.textContent = parseInt(ties.textContent) + 1;
+       }
+}
+
+//Create function with one parameter, playerInput, that plays a round
 let playRound = (playerInput) => {
     //Create variable computerSelection that stores value of getComputerChoice
-    console.log(`YOU CHOSE: ${playerInput}`)
     let computerSelection = getComputerChoice();
-    console.log( "The computer chose: " + computerSelection);
     
     //Create possible win and loss conditions as per rock/paper/scissors game's rules
     if (computerSelection.toLowerCase() === playerInput) {
@@ -76,97 +94,13 @@ let playRound = (playerInput) => {
     } else if ( computerSelection === "Scissors" & playerInput === "paper") {
         results.textContent ="You lose! Scissors beats paper!"
     }
-    if (results.textContent.includes("win")) {
-        playerScore.textContent = parseInt(playerScore.textContent) + 1
-       } else if (results.textContent.includes("lose")) {
-        computerScore.textContent = parseInt(computerScore.textContent) + 1;
-       } else { 
-        ties.textContent = parseInt(ties.textContent) + 1;
-       }
-
-       console.log(`results value at end of round: ${results}`)
-    checkIfGameOver(playerScore.textContent, computerScore.textContent);
+    updateScore();
+    checkIfGameOver();
 }
-
-//Create function checkIfPlayerChoiceValid with two parameters: one for playerSelection, one for initialValidityOfPlayerChoice to check if player input is valid
-let checkIfPlayerChoiceValid = (playerSelection, initialValidityOfPlayerChoice) => {
-    //Create var isNewChoiceValid and init to initialValidityOfPlayerChoice
-    let isNewChoiceValid = initialValidityOfPlayerChoice;
-    //Create if conditionals that will check to see if playerSelection is either 'rock' 'paper' or 'scissors' and will return isNewChoiceValid as true if so; else
-    //return false
-    if (playerSelection === "rock") {
-        return isNewChoiceValid = true;  
-      } else if (playerSelection === "paper") {
-          return isNewChoiceValid = true;
-      } else if (playerSelection === "scissors") {
-          return isNewChoiceValid = true;
-      } return false;
-}
-
-let playGame = () => {
-    //Declare var computerWins, playerWins, and ties to keep track of score
-    let computerWins = 0;
-    let playerWins = 0;
-    let ties = 0;
-    //Iterate through a certain amount of rounds
-    for (let i = 0; i < 5; i++) {
-        //Prompt user to input a string of rock, paper, or scissors and store value as lower case string (to bypass case sensitivity) in playerSelection
-        let playerSelection = prompt("Please type Rock, Paper, or Scissors: ").toLowerCase();
-        //Declare var isPlayerSelectionValid and initialize to false
-        let isPlayerSelectionValid = false;
-        //Check to see if playerSelection is valid
-        isPlayerSelectionValid = checkIfPlayerChoiceValid(playerSelection, isPlayerSelectionValid);
-        
-        //Create while loop that prompts user for valid input if isPlayerSelectionValid is false
-        while (isPlayerSelectionValid === false) {
-            playerSelection = prompt('No, you goofnut! You can only type Rock, Paper, or Scissors.').toLowerCase();
-            isPlayerSelectionValid = checkIfPlayerChoiceValid(playerSelection, isPlayerSelectionValid);
-        }
-        //Display current round number
-        console.log(`Round ${i + 1}:`)
-        //Generate computerSelection for this round
-        let computerSelection = getComputerChoice();
-        //Store roundResult
-        let roundResult = playRound(playerSelection, computerSelection);
-        //Display results of this round
-       console.log(roundResult);
-       //Increase playerWins, computerWins, or ties by 1 depending on the victor of the round
-       if (roundResult.includes("win")) {
-        playerWins += 1;
-       } else if (roundResult.includes("lose")) {
-        computerWins += 1;
-       } else { 
-        ties += 1;
-       }
-       //Display message displaying current values of playerWins, computerWins, and ties
-       console.log(`You have won ${playerWins} rounds. 
-       The computer has won ${computerWins} rounds.
-       There are ${ties} ties.`)
-    }
-    //Display a message declaring the winner of the game
-    if (computerWins > playerWins) {
-        console.log(`You lost the game!`)
-    } else if (playerWins > computerWins) {
-        console.log(`You won the game!`)
-    } else {
-        console.log('Nobody won the game. It\'s a tie!');
-    }
-}
-
-let initializeResults = () => {
-    let results = document.querySelector(".results");
-    return results;
-}
-
-let initializePlayerChoices = () => {
-    let playerChoices = document.querySelectorAll('#playerChoice');
-    playerChoices.forEach(choice => choice.addEventListener('click', () => playRound(choice.className)));
-    return playerChoices;
-}
-
 
 
 let results = document.querySelector(".results");
+results.textContent = "LETS GET READY TO ROCK!!!! (or paper, or scissors)"
 
 let playerChoices = document.querySelectorAll('#playerChoice');
 playerChoices.forEach(choice => choice.addEventListener('click', () => playRound(choice.className)));
